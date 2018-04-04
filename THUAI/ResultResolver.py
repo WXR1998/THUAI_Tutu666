52771db842c3bb4cf55f1377f0afddaab2c94afb
Xuanrun Wang <810158407@qq.com>
2018-04-05 03:41:36 +0800

    upd

--- THUAI/ResultResolver.py
+++ THUAI/ResultResolver.py
@@ -1,6 +1,15 @@
 import os
+def hp(a):
+    s = str()
+    for i in range(int(a)//5):
+        s = s + '#'
+    for i in range(20 - int(a)//5):
+        s = s + ' '
+    return s
+
 BD_NAME = [ '__Base', 'Shannon', 'Thevenin', 'Norton', 'Von_Neumann', 'Berners_Lee', 'Kuen_Kao', 'Turing', 'Tony_Stark', 'Bool', 'Ohm', 'Mole', 'Monte_Carlo', 'Larry_Roberts', 'Robert_Kahn', 'Musk', 'Hawkin', 'Programmer']
 FULL_HP = [10000, 15000, 20000, 25000, 30000, 35000]
+HP = [10000, 150, 200, 180, 200, 150, 160, 250, 220, 200, 320, 250, 350, 220, 520, 1000, 360, 100]
 if __name__ == "__main__":
     num = input("File Number: ")
     try:
@@ -24,7 +33,7 @@ if __name__ == "__main__":
             outf.write('Status %d:\n\tMoney = %8d\tTech = %d\n' % (i, D['status_%d'%i]['money'], D['status_%d'%i]['tech']))
         outf.write('\n')
         for i in range(0, 2):
-            outf.write('Base %d:\n\tHP = %8f / %6.1f per.\n' % (i, D['mainbase_%d'%i]['base_HP'], D['mainbase_%d'%i]['base_HP']/FULL_HP[D['status_%d'%i]['tech']]*100))
+            outf.write('Base %d:\n\tHP = %8.0f|%s|\n' % (i, D['mainbase_%d'%i]['base_HP'], hp(D['mainbase_%d'%i]['base_HP']/FULL_HP[D['status_%d'%i]['tech']]*100)))
         outf.write('\n')
         for i in range(0, 2):
             outf.write('Unit %d: [%4d]\n' % (i, len(D['unit_%d' % i])))
@@ -36,7 +45,7 @@ if __name__ == "__main__":
             for typ in D['buildings_%d' % i]:
                 outf.write('\t%s: [%4d]\n' % (typ, len(D['buildings_%d' % i][typ])))
                 for bd in D['buildings_%d' % i][typ]:
-                    outf.write('\t\t%8d\t%12s\t%8.0f\t%12s\t%4d\n' % (bd['id'], BD_NAME[int(bd['type'])], bd['hp'], str(bd['pos']), bd['level']))
+                    outf.write('\t\t%8d\t%12s\t%8.0f|%s|\t%12s\t%4d\n' % (bd['id'], BD_NAME[int(bd['type'])], bd['hp'], hp(100*bd['hp']/(HP[int(bd['type'])] * (1+0.5*bd['level']))), str(bd['pos']), bd['level']))
         outf.write('\n')
         for i in range(0, 2):
             outf.write('Try to Build %d:\n' % i)
